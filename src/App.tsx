a4ef2a1bca90e8da13201c3ac266d097a4d7b4c9
Park Gyeongsil <59197297+GPS-siri@users.noreply.github.com>
2025-09-16 23:58:19 +0900

    feat: yarn berry 세팅 파일 추가
    - yarn berry 사용을 위한 package.json / .yarn 폴더 / .pnp.* 파일 추가
    - yarn berry 의 Zero-Installs 활용을 위한 .gitignore 파일 수정
    - fix: tailwind 라이브러리 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import FractalTree from './FractalTree';
-import AboutPage from './pages/AboutPage';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import FractalTree from "./FractalTree";
+import AboutPage from "./pages/AboutPage";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <nav style={{ marginBottom: '1rem', borderBottom: '1px solid #ccc', paddingBottom: '1rem' }}>
-        <Link to="/" style={{ marginRight: '1rem' }}>
+      <nav
+        style={{
+          marginBottom: "1rem",
+          borderBottom: "1px solid #ccc",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/" style={{ marginRight: "1rem" }}>
           Fractal Tree
         </Link>
-        <Link to="/about">
-          About
-        </Link>
+        <Link to="/about">About</Link>
       </nav>
 
       <main>
